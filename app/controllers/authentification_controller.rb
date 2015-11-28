16380979b9f7e2dd151ae0135574227ed898493a
Mathieu <mathieu.letyrant@gmail.com>
2015-11-28 18:38:42 +0100

    Prepare form for subscribe and connect

--- app/controllers/authentification_controller.rb
+++ app/controllers/authentification_controller.rb
@@ -1,9 +1,19 @@
 class AuthentificationController < ApplicationController
 
+  # GET /authentification/connect
   def connect
   end
 
+  # POST /authentification/connect
+  def login
+  end
+
+  # GET /authentification/subscribe
   def subscribe
+  end
+
+  # POST /authentification/subscribe
+  def create
   end
 
 end
