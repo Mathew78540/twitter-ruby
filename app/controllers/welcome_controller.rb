1bb428016fcdde3df2a2ed4fb8515c99facb855b
Mathieu <mathieu.letyrant@gmail.com>
2015-11-29 17:35:34 +0100

    If session, redirect / to /dashboard

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,10 @@
 class WelcomeController < ApplicationController
 
   def index
+    # If we have a session, dirirect to the dashboard
+    if self.current_account
+      redirect_to '/dashboard'
+    end
   end
 
 end
