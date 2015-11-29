048e8c2848726337b138a830b385549021c89cfb
Mathieu <mathieu.letyrant@gmail.com>
2015-11-29 18:24:16 +0100

    Add a new tweet

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,8 +5,16 @@ class DashboardController < ApplicationController
   # GET|POST /dashboard
   def index
     if request.post?
+      @current_account.tweet.create(tweet_params)
       # TODO Must be create the tweet ...
     end
+
+    @tweets = @current_account.tweet
+  end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:text)
   end
 
 end
