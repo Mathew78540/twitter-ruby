eac778444b09fcde53e2daca836b2c7ac0e017f0
Mathieu <mathieu.letyrant@gmail.com>
2015-12-03 14:09:54 +0100

    Get all tweets

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -8,8 +8,15 @@ class DashboardController < ApplicationController
       @current_account.tweet.create(tweet_params)
     end
 
-    # TODO Get all tweets from followers
-    @tweets = @current_account.tweet.order('created_at DESC')
+    # Get all tweets
+    following_ids = []
+    following_ids.push(@current_account.id)
+
+    @current_account.following.each do |account|
+      following_ids.push(account.account_id)
+    end
+
+    @tweets = Tweet.where(account_id: following_ids).order('created_at DESC')
   end
 
   private
