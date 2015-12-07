bf539214d017a842dde17c7c521e1f0bfe31cac4
Mathieu <mathieu.letyrant@gmail.com>
2015-12-07 14:08:58 +0100

    SEO for welcome page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,6 +6,9 @@ class WelcomeController < ApplicationController
 
     # If we have a session, redirect to the dashboard :)
     if self.current_account
+
+      @page_description = "Connectez-vous à vos amis — et d'autres personnes fascinantes. Recevez des mises à jour instantanées sur les choses qui vous intéressent. Et regardez les événements se dérouler, en temps réel, sous tous les angles."
+
       return redirect_to '/dashboard'
     end
 
